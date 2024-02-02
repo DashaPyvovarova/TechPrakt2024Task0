3c7706514373e5ca9f83f9ea2f3fcf035f6ee93c
Daria Pyvovarova <vt222_pdv@student.ztu.edu.ua>
2024-02-02 21:57:32 +0200

    add task_1

---
+++ task_1/task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Перше число: ");
+        string input1 = Console.ReadLine();
+
+        Console.Write("Друге число: ");
+        string input2 = Console.ReadLine();
+
+        if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+        {
+            double sum = number1 + number2;
+            Console.WriteLine($"Сума двох чисел: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Некоректні вхідні дані. Введіть дійсні числа.");
+        }
+
+        Console.ReadLine();
+    }
+}
+
